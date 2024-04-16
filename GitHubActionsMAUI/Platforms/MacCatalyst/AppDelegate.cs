267295250c2e0fdd8fbfc54fc74d0b00624401f1
Zuber Shaikh <zuber1234@gmail.com>
2024-04-16 15:55:05 +0530

    Initial Commit

---
+++ GitHubActionsMAUI/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace GitHubActionsMAUI;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
+
